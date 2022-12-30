c4a56358f90211d30b526946ea73e215e2cf0cba
keeynnyth <kedonado@hotmail.com>
2022-12-30 19:11:16 -0300

    meme

---
+++ memegenerador/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Textomeme from './componentes/Textomeme';
+import Imgmeme from './componentes/Imgmeme';
+function App() {
+  return (
+    <div className="App">
+    
+    <Textomeme/>
+     <Imgmeme/>
+    </div>
+  );
+}
+
+
+export default App;
